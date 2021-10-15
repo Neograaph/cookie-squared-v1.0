becbf0b81ff40fee95546b451ed968e7675ebf89
Maxime Gauthier <max45270@gmail.com>
2021-10-15 16:26:06 +0200

    finMenuBurger

--- assets/js/nav.js
+++ assets/js/nav.js
@@ -1,12 +1,18 @@
 console.log('annimation nav burger');
 let navStatus = false;
-document.getElementById('navClose').style.display="none";
+let navOpen = false;
+// display none au chargement de la page
+// document.getElementById('navClose').style.display="none";
+// document.getElementById('navOpen').style.display="block";
 
+// events pour masquer/afficher au click
 document.getElementById('navShow').addEventListener('click', function () {
   navStatus = !navStatus;
   console.log(navStatus);
   document.getElementById('navShow').style.display="none";
   document.getElementById('navClose').style.display="block";
+  document.getElementById('navOpen').style.display="block";
+
 });
 
 document.getElementById('navClose').addEventListener('click', function () {
@@ -14,4 +20,6 @@ document.getElementById('navClose').addEventListener('click', function () {
   console.log(navStatus);
   document.getElementById('navShow').style.display="block";
   document.getElementById('navClose').style.display="none";
+  document.getElementById('navOpen').style.display="none";
+
 });
