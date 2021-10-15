f1d17f80b42c14014d309e285d2f619e76647cdf
Maxime Gauthier <max45270@gmail.com>
2021-10-15 15:35:29 +0200

    iconsDynamique

--- assets/js/nav.js
+++ assets/js/nav.js
@@ -1,6 +1,17 @@
 console.log('annimation nav burger');
 let navStatus = false;
+document.getElementById('navClose').style.display="none";
+
 document.getElementById('navShow').addEventListener('click', function () {
   navStatus = !navStatus;
   console.log(navStatus);
-})
+  document.getElementById('navShow').style.display="none";
+  document.getElementById('navClose').style.display="block";
+});
+
+document.getElementById('navClose').addEventListener('click', function () {
+  navStatus = !navStatus;
+  console.log(navStatus);
+  document.getElementById('navShow').style.display="block";
+  document.getElementById('navClose').style.display="none";
+});
