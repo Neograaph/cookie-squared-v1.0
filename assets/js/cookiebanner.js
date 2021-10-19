127093de2c4bf12b2cc987e04fd1ad1a07ea6110
Clément Piquenet <clement.piquenet@gmail.com>
2021-10-19 17:14:45 +0200

    probleme aside

--- assets/js/cookiebanner.js
+++ assets/js/cookiebanner.js
@@ -1,10 +1,47 @@
 
+let cookiebanner = document.getElementById('cookiebanner');
+// let main = document.getElementById('main');
+// let nav = document.getElementById('nav');
+// let footer = document.getElementById('footer');
+let aside = document.getElementById('aside');
+let showPopup = false;
+console.log(showPopup);
+
 
 document.getElementById('cookie').addEventListener('click', function () {
-    
-    document.getElementById('cookiebanner').style.display="block";
-    document.getElementById('cookie').style.display="none";
-    
-  });
+   console.log("clique cookie");
+   document.getElementById('cookiebanner').style.display="block";
+   document.getElementById('cookie').style.display="none";
+   showPopup = true;
+   console.log(showPopup);
+});
+window.addEventListener('click', function (event) {
+    if (event.target != cookiebanner){
+        cookiebanner.style.display = "none";
+        showPopup = false;
+        console.log(showPopup);
+    }
 
+})
+// if (showPopup == true){
+//     // test ();
+//     console.log('condition true ');
+// }
+
+// // function test () {
+//         window.onclick = function(event) {
+//         console.log(showPopup);
+//         console.log('here');
+//         console.log(event.target);
+//     }
+// }
+// };
+  // if (event.target == document.getElementById('footer')){
+      //   cookiebanner.style.display = "none";
+      //   console.log("test");
+      // };
+      
+      // if (event.target == nav){
+          //   cookiebanner.style.display = "none";
+          // };
 
