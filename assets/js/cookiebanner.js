b192081d5247dc583d5c1dda51b5ea8564d25a7c
Clément Piquenet <clement.piquenet@gmail.com>
2021-10-19 16:07:07 +0200

    premier essai animation

---
+++ assets/js/cookiebanner.js
@@ -0,0 +1,10 @@
+
+
+document.getElementById('cookie').addEventListener('click', function () {
+    
+    document.getElementById('cookiebanner').style.display="block";
+    document.getElementById('cookie').style.display="none";
+    
+  });
+
+
