6774cece4cb338efb3fab3becef5f1fb65a90397
Maxime Gauthier <max45270@gmail.com>
2021-10-19 15:22:13 +0200

    ajout du bouton revenir en haut

--- assets/app.js
+++ assets/app.js
@@ -16,8 +16,11 @@ import './styles/accueil.css';
 import './styles/forms.css';
 import './styles/userpage.css';
 import './styles/footer.css';
+import './styles/backToTop.css';
+
 import './js/slider';
 import './js/nav.js';
+import './js/backToTop';
 
 // start the Stimulus application
 import './bootstrap';
