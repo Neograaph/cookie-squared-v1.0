7bd6427fda47d555ffb2a6046d07f10a4fce4013
Clément Piquenet <clement.piquenet@gmail.com>
2021-10-20 14:50:38 +0200

    add banniere2

--- assets/js/cookiebanner.js
+++ assets/js/cookiebanner.js
@@ -3,8 +3,13 @@ let cookiebanner = document.getElementById('cookiebanner');
 let cookie = document.getElementById('cookie');
 let all = document.getElementById('all');
 let aside = document.getElementById('aside');
+let btnrefuse = document.getElementById('btnrefuse');
+let btnpref = document.getElementById('btnpref');
+let btnaccept = document.getElementById('btnaccept');
+let banniere2 = document.getElementById('banniere2');
 
 
+// ----------banniere1-------------
 
 document.getElementById('cookie').addEventListener('click', function () {
    cookiebanner.style.display="block";
@@ -20,3 +25,33 @@ window.addEventListener('click', function (event) {
     }
 
 })
+
+
+// --------banniere2-----------------
+
+document.getElementById('btnrefuse').addEventListener('click', function () {
+    cookie.style.display="block";
+    banniere2.style.display="none"
+    all.style.display="none";
+
+ });
+ 
+
+document.getElementById('btnpref').addEventListener('click', function () {
+    
+    banniere2.style.display="none"
+    cookiebanner.style.display="block";
+    cookie.style.display="none";
+    all.style.display="block";
+    
+
+
+ });
+
+document.getElementById('btnaccept').addEventListener('click', function () {
+    cookie.style.display="block";
+    banniere2.style.display="none"
+    all.style.display="none";
+
+
+ });
