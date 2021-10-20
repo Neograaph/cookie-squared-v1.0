360e6155ee8383a659dce4147f9d8be11ceef21a
Clément Piquenet <clement.piquenet@gmail.com>
2021-10-20 12:00:44 +0200

    creation de la banniere de demarrage

--- assets/js/cookiebanner.js
+++ assets/js/cookiebanner.js
@@ -1,51 +1,22 @@
 
 let cookiebanner = document.getElementById('cookiebanner');
-// let main = document.getElementById('main');
-// let nav = document.getElementById('nav');
-// let footer = document.getElementById('footer');
+let cookie = document.getElementById('cookie');
 let all = document.getElementById('all');
 let aside = document.getElementById('aside');
-let showPopup = false;
-console.log(showPopup);
+
 
 
 document.getElementById('cookie').addEventListener('click', function () {
-   console.log("clique cookie");
-   document.getElementById('cookiebanner').style.display="block";
-   document.getElementById('cookie').style.display="none";
+   cookiebanner.style.display="block";
+   cookie.style.display="none";
    all.style.display="block";
-   showPopup = true;
-   console.log(showPopup);
 });
+
 window.addEventListener('click', function (event) {
     if (event.target == document.getElementById('all')){
         cookiebanner.style.display = "none";
-        showPopup = false;
-        console.log(showPopup);
-        document.getElementById('all').style.display="none";
-        document.getElementById('cookie').style.display="block";
+        all.style.display="none";
+        cookie.style.display="block";
     }
 
 })
-// if (showPopup == true){
-//     // test ();
-//     console.log('condition true ');
-// }
-
-// // function test () {
-//         window.onclick = function(event) {
-//         console.log(showPopup);
-//         console.log('here');
-//         console.log(event.target);
-//     }
-// }
-// };
-  // if (event.target == document.getElementById('footer')){
-      //   cookiebanner.style.display = "none";
-      //   console.log("test");
-      // };
-      
-      // if (event.target == nav){
-          //   cookiebanner.style.display = "none";
-          // };
-
