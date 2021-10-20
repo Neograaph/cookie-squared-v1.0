8fa5259646f4d8d8ff988e753ac54bb2c5cd3f3d
Clément Piquenet <clement.piquenet@gmail.com>
2021-10-20 11:12:43 +0200

    css et js ok

--- assets/js/cookiebanner.js
+++ assets/js/cookiebanner.js
@@ -3,6 +3,7 @@ let cookiebanner = document.getElementById('cookiebanner');
 // let main = document.getElementById('main');
 // let nav = document.getElementById('nav');
 // let footer = document.getElementById('footer');
+let all = document.getElementById('all');
 let aside = document.getElementById('aside');
 let showPopup = false;
 console.log(showPopup);
@@ -12,14 +13,17 @@ document.getElementById('cookie').addEventListener('click', function () {
    console.log("clique cookie");
    document.getElementById('cookiebanner').style.display="block";
    document.getElementById('cookie').style.display="none";
+   all.style.display="block";
    showPopup = true;
    console.log(showPopup);
 });
 window.addEventListener('click', function (event) {
-    if (event.target != cookiebanner){
+    if (event.target == document.getElementById('all')){
         cookiebanner.style.display = "none";
         showPopup = false;
         console.log(showPopup);
+        document.getElementById('all').style.display="none";
+        document.getElementById('cookie').style.display="block";
     }
 
 })
