e0228df73bb6d9cf77de6488e2e2d7ee221ef833
Antoine Mongazon <87534417+AntoineMadHat4@users.noreply.github.com>
2021-10-21 14:04:08 +0200

    ajout de la page de gestion des cookies et création du formulaire d'ajout

--- src/Controller/CookieController.php
+++ src/Controller/CookieController.php
@@ -2,19 +2,38 @@
 
 namespace App\Controller;
 
-use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use App\Entity\Cookie;
+use App\Form\CookieType;
+use App\Repository\CookieRepository;
+use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
 class CookieController extends AbstractController
 {
     /**
-     * @Route("/dashboard/cookie", name="cookie")
+     * @Route("/dashboard/cookie", name="cookie", methods="GET|POST")
      */
-    public function cookieList(): Response
+    public function cookieList(CookieRepository $cookieRepository, Request $request, EntityManagerInterface $em): Response
     {
+        $cookies = $cookieRepository->findAll();
+
+        $cookie = new Cookie;
+
+        $form = $this->createForm(CookieType::class, $cookie);
+        $form->handleRequest($request);
+        if ($form->isSubmitted() && $form->isValid()) {
+            $em->persist($cookie);
+            $em->flush();
+        }
+
+
         return $this->render('dashboard/cookielist.html.twig', [
             'controller_name' => 'CookieController',
+            'cookies' => $cookies,
+            'cookieForm' => $form->createView()
         ]);
     }
 }
