f0a1369c62c3ace4f4103ad1411d09179098c952
Maxime Gauthier <max45270@gmail.com>
2021-10-22 16:38:54 +0200

    nav.js + custom

--- assets/js/nav.js
+++ assets/js/nav.js
@@ -1,32 +1,33 @@
-console.log('annimation nav burger');
+// console.log('annimation nav burger');
 let navStatus = false;
 let navOpen = false;
-// display none au chargement de la page
-// document.getElementById('navClose').style.display="none";
-// document.getElementById('navOpen').style.display="block";
 
 // events pour masquer/afficher au click
 document.getElementById('navShow').addEventListener('click', function () {
   navStatus = !navStatus;
-  console.log(navStatus);
+  // console.log(navStatus);
+  console.log(location.pathname);
   document.getElementById('navShow').style.display="none";
   document.getElementById('navClose').style.display="block";
   document.getElementById('navOpen').style.display="block";
-  document.getElementById('main').style.display="none";
-  document.getElementById('footer').style.display="none";
-  document.getElementById('aside').style.display="none";
-
+  if (location.pathname == '/'){
+    document.getElementById('main').style.display="none";
+    document.getElementById('footer').style.display="none";
+    document.getElementById('aside').style.display="none";
+  }
 });
 
 document.getElementById('navClose').addEventListener('click', function () {
   navStatus = !navStatus;
-  console.log(navStatus);
+  // console.log(navStatus);
   document.getElementById('navShow').style.display="block";
   document.getElementById('navClose').style.display="none";
   document.getElementById('navOpen').style.display="none";
+  if (location.pathname == '/'){
   document.getElementById('main').style.display="block";
   document.getElementById('footer').style.display="block";
   document.getElementById('aside').style.display="block";
+  }
 });
 
 document.getElementsByClassName('link')[0].addEventListener('click', function(){hide()});
