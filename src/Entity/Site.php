299affcb3b57f2d2dea7760f27cb328afe7bc349
Maxime Gauthier <max45270@gmail.com>
2021-11-03 14:04:55 +0100

    clean

--- src/Entity/Site.php
+++ src/Entity/Site.php
@@ -57,6 +57,11 @@ class Site
      */
     private $cookies;
 
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $token;
+
     public function __construct()
     {
         $this->created_at = new DateTimeImmutable();
@@ -167,6 +172,18 @@ class Site
                 $cookie->setIdSite(null);
             }
         }
+
+        return $this;
+    }
+
+    public function getToken(): ?string
+    {
+        return $this->token;
+    }
+
+    public function setToken(string $token): self
+    {
+        $this->token = $token;
 
         return $this;
     }
