89ad1509e4ee8f868e19c03ee5a775ae901cec64
Maxime Gauthier <max45270@gmail.com>
2021-11-03 16:03:46 +0100

    init test

---
+++ src/Controller/RequestController.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class RequestController extends AbstractController
+{
+    /**
+     * @Route("/dashboard/request", name="request")
+     */
+    public function index(): Response
+    {
+        return $this->render('request/cookiesList.JSON');
+    }
+
+}
