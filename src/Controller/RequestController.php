a73a47c3baae85503c460c86ab5b0402ec0dbbfc
Maxime Gauthier <max45270@gmail.com>
2021-11-05 15:40:50 +0100

    clean

--- src/Controller/RequestController.php
+++ src/Controller/RequestController.php
@@ -2,24 +2,37 @@
 
 namespace App\Controller;
 
-use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
-use Symfony\Component\HttpFoundation\JsonResponse;
+use App\Entity\Site;
+use App\Repository\CookieRepository;
+use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Component\HttpFoundation\Cookie;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
 class RequestController extends AbstractController
 {
     /**
      * @Route("/request", name="request")
      */
-    public function index(): Response
+    public function cookieShow(CookieRepository $cookieRepository): Response
     {
-        // return $this->render('request/cookiesList.json');
+        $cookieslist = $cookieRepository->findAll();
+        return $this->render('request/cookiesList.html.twig', compact('cookieslist'));
+    }
+    /**
+     * @Route("/request/json", name="request-json")
+     */
+    public function jsonShow(CookieRepository $cookieRepository): Response
+    {
+        $cookieslist = $cookieRepository->findAll();
         header("Access-Control-Allow-Origin: *");
         return new JsonResponse([
             'cookie1' => "nuit",
             'cookie2' => "blabla",
-            'cookie3' => "bonjour",
+            'cookie3' => $cookieslist,
         ]);
     }
 }
