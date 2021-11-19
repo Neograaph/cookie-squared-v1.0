6fcea5ab49d91514a0120f188885ce56d9a60741
Maxime Gauthier <max45270@gmail.com>
2021-11-19 18:09:01 +0100

    clean

--- src/Controller/WordpressRequestController.php
+++ src/Controller/WordpressRequestController.php
@@ -3,12 +3,15 @@
 namespace App\Controller;
 
 use App\Entity\Site;
+use App\Entity\WordpressSite;
+use Doctrine\ORM\EntityManager;
 use App\Repository\SiteRepository;
 use App\Repository\CookieRepository;
 use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Component\HttpFoundation\Response;
+use App\Repository\WordpressSiteRepository;
 use Symfony\Component\HttpFoundation\Cookie;
 use Symfony\Component\HttpFoundation\Request;
-use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
@@ -25,7 +28,7 @@ class WordpressRequestController extends AbstractController
         return $this->render('request/cookiesList.html.twig', compact('cookieslist'));
     }
     /**
-     * @Route("/request/cookies/json", name="request-json")
+     * @Route("/request/cookies/json", name="request-allcookies")
      */
     public function cookiesJsonShow(CookieRepository $cookieRepository): Response
     {
@@ -36,7 +39,7 @@ class WordpressRequestController extends AbstractController
         );
     }
     /**
-     * @Route("/request/cookies/{token}", name="request-token")
+     * @Route("/request/cookies/{token}", name="request-tokencookies")
      */
     public function cookiesTokenJsonShow($token, CookieRepository $cookieRepository, SiteRepository $siterepo): Response
     {
@@ -46,5 +49,22 @@ class WordpressRequestController extends AbstractController
         return $this->json(
             $cookieslist,200,[],["groups"=>'displayCookie']
         );
+    }
+    /**
+     * @Route("/request/push/banner/{key}", name="request-push-banner")
+     */
+    public function bannerKeyPush($key, WordpressSiteRepository $WordpressSiteRepository, EntityManagerInterface $em): Response
+    {
+        header("Access-Control-Allow-Origin: *");
+        $site = new WordpressSite;
+        $site->setUrl($key);
+        $site->setToken('params.token');
+        $site->setTitle('');
+        $site->setDescription('');
+        $site->setColor('');
+        $em->persist($site);
+        $em->flush();
+        
+        return $this->json($key);
     }
 }
